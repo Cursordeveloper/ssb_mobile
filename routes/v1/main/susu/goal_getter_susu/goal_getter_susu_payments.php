c6617eb3fad7572706485371de36969e9e309f6c
Michael <billyston@gmail.com>
2024-07-01 08:25:49 +0000

    GoalGetterSusu: payment_cancel feature done.

--- routes/v1/main/susu/goal_getter_susu/goal_getter_susu_payments.php
+++ routes/v1/main/susu/goal_getter_susu/goal_getter_susu_payments.php
@@ -3,13 +3,18 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\V1\Susu\GoalGetterSusu\Payment\GoalGetterSusuPaymentApprovalController;
+use App\Http\Controllers\V1\Susu\GoalGetterSusu\Payment\GoalGetterSusuPaymentCancellationController;
 use App\Http\Controllers\V1\Susu\GoalGetterSusu\Payment\GoalGetterSusuPaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'customers/susus/goal-getter-susus', 'as' => 'customers.susus.goal-getter-susus.', 'middleware' => 'auth:customer'], function (): void {
-    // GoalGetterSusu payment
+    // GoalGetterSusu payment route
     Route::post(uri: '{susu}/payments', action: GoalGetterSusuPaymentController::class)
         ->name(name: 'payments.store');
+
+    // GoalGetterSusu payment cancellations route
+    Route::post(uri: '{susu}/payments/{payment}/cancellations', action: GoalGetterSusuPaymentCancellationController::class)
+        ->name(name: 'payments.cancellations.update');
 
     // GoalGetterSusu payment approval
     Route::post(uri: '{susu}/payments/{payment}/approvals', action: GoalGetterSusuPaymentApprovalController::class)
