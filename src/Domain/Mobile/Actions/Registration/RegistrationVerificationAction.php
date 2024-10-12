bd8922566eb570448a2e213a96ad220387bf7e13
Michael <billyston@gmail.com>
2024-10-12 15:07:20 +0000

    Registration: Phone number verification modified

--- src/Domain/Mobile/Actions/Registration/RegistrationVerificationAction.php
+++ src/Domain/Mobile/Actions/Registration/RegistrationVerificationAction.php
@@ -6,11 +6,11 @@ namespace Domain\Mobile\Actions\Registration;
 
 use App\Common\ResponseBuilder;
 use App\Http\Resources\V1\Mobile\Registration\RegistrationResource;
+use Domain\Customer\Services\Registration\CustomerByNumberService;
+use Domain\Customer\Services\Registration\CustomerCreateService;
 use Domain\Mobile\Enums\CustomerStatus;
 use Domain\Mobile\Jobs\Registration\RegistrationVerificationJob;
 use Domain\Mobile\Models\Customer;
-use Domain\Shared\Exceptions\Registration\SystemFailureExec;
-use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -19,40 +19,43 @@ final class RegistrationVerificationAction
     public static function execute(array $request): JsonResponse
     {
         // Get the customer
-        $customer = Customer::where(column: 'phone_number', operator: '=', value: data_get(target: $request, key: 'data.attributes.phone_number'))->first();
+        $customer = CustomerByNumberService::execute(request: $request);
 
-        // Return the [Incomplete registration] if customer has no password
-        if (! empty($customer) && empty($customer->password)) {
-            return ResponseBuilder::resourcesResponseBuilder(
-                status: false,
-                code: Response::HTTP_PARTIAL_CONTENT,
-                message: 'Incomplete registration.',
-                description: 'You have not created a password for this account.',
-                data: new RegistrationResource(resource: $customer),
-            );
-        }
+        // Evaluate the $customer and take appropriate action
+        return match (true) {
+            $customer === null => self::customerCreate(request: $request),
+            $customer->status === CustomerStatus::Pending->value => self::customerToken(customer: $customer),
+            $customer->password === null => ResponseBuilder::resourcesResponseBuilder(status: false, code: Response::HTTP_PARTIAL_CONTENT, message: 'Incomplete registration.', description: 'You have not created a password for this account.', data: new RegistrationResource(resource: $customer)),
 
-        // Return the [Incomplete registration] response if phone is used
-        if (! empty($customer) && $customer->status === CustomerStatus::Active->value) {
-            return ResponseBuilder::resourcesResponseBuilder(
+            default => ResponseBuilder::resourcesResponseBuilder(
                 status: false,
                 code: Response::HTTP_UNPROCESSABLE_ENTITY,
                 message: 'Request unprocessable.',
-                description: 'The phone number is already taken.',
-            );
-        }
+                description: 'The phone number is already taken.'
+            ),
+        };
+    }
 
+    private static function customerCreate(array $request): JsonResponse
+    {
         // Create the new customer
-        $customer = Customer::create([
-            'resource_id' => Str::uuid()->toString(),
-            'phone_number' => data_get(target: $request, key: 'data.attributes.phone_number'),
-        ]);
+        $customer = CustomerCreateService::execute(request: $request);
 
-        // Return RegistrationVerificationExc
-        if (! $customer) {
-            throw new SystemFailureExec;
-        }
+        // Dispatch RegistrationVerificationJob
+        RegistrationVerificationJob::dispatch($customer);
 
+        // Return the resourceResponseBuilder
+        return ResponseBuilder::resourcesResponseBuilder(
+            status: true,
+            code: Response::HTTP_OK,
+            message: 'Request successful.',
+            description: 'Phone number verification in progress. You will be notified shortly.',
+            data: new RegistrationResource($customer),
+        );
+    }
+
+    private static function customerToken(Customer $customer): JsonResponse
+    {
         // Dispatch RegistrationVerificationJob
         RegistrationVerificationJob::dispatch($customer);
 
